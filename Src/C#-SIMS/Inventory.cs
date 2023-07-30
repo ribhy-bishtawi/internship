5ac94c6133f02129fb8d02b347b0e07a3dac6388
ribhy-bishtawi <ribhy2001@gmail.com>
2023-07-30 17:25:58 +0300

    [Add Products] Implement product creation feature
    -Implemented the UI for adding product details.

--- Src/C#-SIMS/Inventory.cs
+++ Src/C#-SIMS/Inventory.cs
@@ -4,10 +4,15 @@ class Inventory
 {
     private static List<Product> inventory = new();
 
-    internal static void ShowMenu()
+    private static void initUI()
     {
         Console.ResetColor();
         Console.Clear();
+    }
+
+    internal static void ShowMenu()
+    {
+        initUI();
         Console.WriteLine("****Simple Inventory Management System****");
         Console.WriteLine("Please Select an action to perform");
         Console.WriteLine("******************************************");
@@ -19,5 +24,43 @@ class Inventory
         Console.WriteLine("6:Exit");
 
         string? chosenOption = Console.ReadLine();
+
+        switch (chosenOption)
+        {
+            case "1":
+                AddProduct();
+                break;
+            default:
+                Console.WriteLine("Invalid. Please try again.");
+                break;
+        }
+    }
+
+    private static void AddProduct()
+    {
+        initUI();
+        int quantity,
+            price;
+        bool parseSuccess;
+        string? userSelection,
+            userName;
+        do
+        {
+            Console.WriteLine("****Add a product****");
+            Console.Write("Enter the product name: ");
+            userName = Console.ReadLine();
+            Console.Write("Enter the product price: ");
+            parseSuccess = int.TryParse(Console.ReadLine(), out int productPrice);
+            price = parseSuccess ? productPrice : 0;
+            Console.Write("Enter the product quantity: ");
+            parseSuccess = int.TryParse(Console.ReadLine(), out int productQuantity);
+            quantity = parseSuccess ? productQuantity : 0;
+            new Product(userName, price, quantity);
+            Console.Write(
+                "The product added successfully.....\nPress 1 to continue or 0 to exit to the main menu: "
+            );
+            userSelection = Console.ReadLine();
+        } while (userSelection != "0");
+        ShowMenu();
     }
 }
--- Src/C#-SIMS/Product.cs
+++ Src/C#-SIMS/Product.cs
@@ -2,6 +2,13 @@ namespace C__SIMS;
 
 class Product
 {
+    public Product(string name, int price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+
     public string? Name { get; set; }
     public int Price { get; set; }
     public int Quantity { get; set; }
