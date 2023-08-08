b0d38f67674fffa9253e1a96262c89864d76f6ec
ribhy-bishtawi <ribhy2001@gmail.com>
2023-08-08 15:33:02 +0300

    Manager view the booked flights of all passengers

--- Src/C#-airport_ticket_booking_sys/Models/Passenger.cs
+++ Src/C#-airport_ticket_booking_sys/Models/Passenger.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AirportTrackingSystem.Models;
 public class Passenger
 {
 
-
+    [Required(ErrorMessage = "Name is required.")]
     public string Name { get; set; }
+
+    [Required(ErrorMessage = "Password is required.")]
     public string Password { get; set; }
     public List<Flight> Flights { get; set; }
 }
