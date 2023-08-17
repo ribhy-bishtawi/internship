03fd7cb5d6aeb1926a28a1752e6afff092158a38
ribhy-bishtawi <ribhy2001@gmail.com>
2023-08-17 14:01:20 +0300

    Observer pattern

--- Src/Real-time weather monitoring and reporting service/Models/Rain_bot.cs
+++ Src/Real-time weather monitoring and reporting service/Models/Rain_bot.cs
@@ -1,11 +1,20 @@
 using System;
 using System.Collections.Generic;
 
-public class RainBot
+public class RainBot : IWeatherObserver
 {
     public bool? Enabled { get; set; }
     public int? HumidityThreshold { get; set; }
     public string? Message { get; set; }
+
+    public void Update(WeatherData weatherData)
+    {
+        if (weatherData.Humidity > HumidityThreshold)
+        {
+            Console.WriteLine("RainBot activated!");
+            Console.WriteLine($"RainBot: \"{Message}\" ");
+        }
+    }
 
 
 
--- Src/Real-time weather monitoring and reporting service/Models/Weather.cs
+++ Src/Real-time weather monitoring and reporting service/Models/Weather.cs
@@ -6,6 +6,25 @@ public class WeatherData
     public string? Location { get; set; }
     public double? Temperature { get; set; }
     public double? Humidity { get; set; }
+    private List<IWeatherObserver> observers = new List<IWeatherObserver>();
+
+    public void Subscribe(IWeatherObserver observer)
+    {
+        observers.Add(observer);
+    }
+
+    public void Unsubscribe(IWeatherObserver observer)
+    {
+        observers.Remove(observer);
+    }
+
+    public void NotifyObservers()
+    {
+        foreach (var observer in observers)
+        {
+            observer.Update(this);
+        }
+    }
 
 
 }
