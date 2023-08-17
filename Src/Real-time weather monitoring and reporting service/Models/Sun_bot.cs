6a4f78e30ed76ceab3fdc41a3737097b0842fd13
ribhy-bishtawi <ribhy2001@gmail.com>
2023-08-17 13:54:00 +0300

    Intial files

---
+++ Src/Real-time weather monitoring and reporting service/Models/Sun_bot.cs
@@ -0,0 +1,10 @@
+
+using System;
+using System.Collections.Generic;
+public class SunBot
+{
+    public bool? Enabled { get; set; }
+    public int? TemperatureThreshold { get; set; }
+    public string? Message { get; set; }
+
+}
---
+++ Src/Real-time weather monitoring and reporting service/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+
+    }
+
+}
+
+
+
+
+
+
