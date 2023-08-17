4e1c7bc77f788f4e00c98430cbfe00f9aa9cf4bf
ribhy-bishtawi <ribhy2001@gmail.com>
2023-08-17 14:03:08 +0300

    Data manipulation

--- Src/Real-time weather monitoring and reporting service/Models/Weather.cs
+++ Src/Real-time weather monitoring and reporting service/Models/Weather.cs
@@ -26,5 +26,40 @@ public class WeatherData
         }
     }
 
+    public string ToJson()
+    {
+        return JsonConvert.SerializeObject(this);
+    }
+
+    public void FromJson(string json)
+    {
+        WeatherData newData = JsonConvert.DeserializeObject<WeatherData>(json)!;
+        Location = newData.Location;
+        Temperature = newData.Temperature;
+        Humidity = newData.Humidity;
+        NotifyObservers();
+
+
+    }
+
+    public string ToXml()
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(WeatherData));
+        StringWriter stringWriter = new StringWriter();
+        serializer.Serialize(stringWriter, this);
+        return stringWriter.ToString();
+    }
+
+    public void FromXml(string xml)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(WeatherData));
+        StringReader stringReader = new StringReader(xml);
+        WeatherData newData = (WeatherData)serializer.Deserialize(stringReader)!;
+        Location = newData.Location;
+        Temperature = newData.Temperature;
+        Humidity = newData.Humidity;
+        NotifyObservers();
+    }
+
 
 }
